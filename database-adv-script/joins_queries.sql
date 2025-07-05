ebf86c72bb33b13d185b39826824de84831409f0
Joshua Honu <65913160+joshua1elson@users.noreply.github.com>
2025-07-05 19:24:23 +0000

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -8,3 +8,26 @@ FROM
     bookings
 INNER JOIN 
     users ON bookings.user_id = users.id;
+
+SELECT 
+    properties.id AS property_id,
+    properties.name AS property_name,
+    reviews.id AS review_id,
+    reviews.rating,
+    reviews.comment
+FROM 
+    properties
+LEFT JOIN 
+    reviews ON properties.id = reviews.property_id;
+
+
+SELECT 
+    users.id AS user_id,
+    users.name AS user_name,
+    bookings.id AS booking_id,
+    bookings.property_id,
+    bookings.booking_date
+FROM 
+    users
+FULL OUTER JOIN 
+    bookings ON users.id = bookings.user_id;
