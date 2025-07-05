e320305417bcc8cb15109821a052b40343f21781
Joshua Honu <65913160+joshua1elson@users.noreply.github.com>
2025-07-05 19:52:57 +0000

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -23,3 +23,25 @@ FROM
 JOIN users u ON b.user_id = u.id
 JOIN properties p ON b.property_id = p.id
 JOIN payments pay ON b.id = pay.booking_id;
+
+
+
+-- EXPLAIN ANALYZE
+SELECT 
+    b.id AS booking_id,
+    b.booking_date,
+    u.id AS user_id,
+    u.name AS user_name,
+    u.email AS user_email,
+    p.id AS property_id,
+    p.name AS property_name,
+    p.location AS property_location,
+    pay.id AS payment_id,
+    pay.amount,
+    pay.payment_method,
+    pay.payment_date
+FROM 
+    bookings b
+JOIN users u ON b.user_id = u.id
+JOIN properties p ON b.property_id = p.id
+JOIN payments pay ON b.id = pay.booking_id;
